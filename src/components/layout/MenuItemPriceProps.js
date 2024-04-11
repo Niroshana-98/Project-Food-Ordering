5c3ff8e3783dafee3b64a3971d2e51c98503e2c4
Niroshana-98 <niroshanthirimadura@gmail.com>
2024-04-11 08:44:37 +0530

    drop down button menu item

--- src/components/layout/MenuItemPriceProps.js
+++ src/components/layout/MenuItemPriceProps.js
@@ -1,7 +1,12 @@
 import Trash from "../icons/Trash";
 import Plus from "../icons/Plus";
+import ChevronDown from "@/components/icons/ChevronDown";
+import ChevronUp from "@/components/icons/ChevronUp";
+import { useState } from "react";
 
 export default function MenuItemPriceProps({name,addLabel,props,setProps}){
+
+    const [isOpen, setIsOpen] = useState(false);
 
     function addProp(){
         setProps(oldProps =>{
@@ -24,48 +29,53 @@ export default function MenuItemPriceProps({name,addLabel,props,setProps}){
 
     return(
         <div className="bg-white p-2 rounded-md mb-2">
-                    <div className="flex gap-1">
+            <button 
+                onClick={() => setIsOpen(prev => !prev)}
+                className="inline-flex p-1 border-0 justify-start" 
+                type="button">
+                    {isOpen && (
+                        <ChevronUp/>
+                    )}
+                    {!isOpen && (
+                        <ChevronDown/>
+                    )}
+                    <span>{name}</span>
+            </button>
+            <div className={isOpen ? 'block' :'hidden'}>
+                {props?.length > 0 && props.map((size,index) => (
+                    <div className="flex items-end gap-2" key={index}>
                         <div>
-                            <button 
-                                className="inline-flex p-1" 
-                                type="button">
-                                    Toggle
+                            <label>Name</label>
+                            <input type="text"
+                                    placeholder="Size Name" 
+                                    value={size.name}
+                                    onChange={ev => editProp(ev, index, 'name')}/>
+                        </div>
+                        <div>
+                            <label>Extra Price</label>
+                            <input type="text"
+                                    placeholder="Extra Price" 
+                                    value={size.price}
+                                    onChange={ev => editProp(ev, index, 'price')}/>
+                        </div>
+                        <div>
+                            <button type="button"
+                                    onClick={() => removeProp(index)}
+                                    className="bg-white mb-2 px-2">
+                                        <Trash/>
                             </button>
                         </div>
-                        <h3 className="grow text-gray-700">{name}</h3>
                     </div>
-                    {props?.length > 0 && props.map((size,index) => (
-                        <div className="flex items-end gap-2" key={index}>
-                            <div>
-                                <label>Name</label>
-                                <input type="text"
-                                        placeholder="Size Name" 
-                                        value={size.name}
-                                        onChange={ev => editProp(ev, index, 'name')}/>
-                            </div>
-                            <div>
-                                <label>Extra Price</label>
-                                <input type="text"
-                                       placeholder="Extra Price" 
-                                       value={size.price}
-                                       onChange={ev => editProp(ev, index, 'price')}/>
-                            </div>
-                            <div>
-                                <button type="button"
-                                        onClick={() => removeProp(index)}
-                                        className="bg-white mb-2 px-2">
-                                            <Trash/>
-                                </button>
-                            </div>
-                        </div>
-                    ))}
-                    <button
-                     type="button"
-                     onClick={addProp}
-                     className="bg-secondary text-white flex items-center justify-center">
-                        <Plus className="w-5 h-5"/>
-                        <span className="ml-2">{addLabel}</span>
-                    </button>
-                </div>
+                ))}
+                <button
+                    type="button"
+                    onClick={addProp}
+                    className="bg-secondary text-white flex items-center justify-center">
+                    <Plus className="w-5 h-5"/>
+                    <span className="ml-2">{addLabel}</span>
+                </button>
+            </div>
+            
+        </div>
     );
 }
